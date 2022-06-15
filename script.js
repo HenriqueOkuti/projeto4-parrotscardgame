7247465a23f2495e84f739a9c540ab4e9677f038
HenriqueOkuti <henrique.krastins@gmail.com>
2022-06-15 09:56:32 -0300

    Added function that chooses the cards that'll be played

--- script.js
+++ script.js
@@ -4,6 +4,10 @@
 
 
 let number_cards;
+let i;
+
+let card_options = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"]
+let card_game = [];
 
 
 /*========================
@@ -17,12 +21,12 @@ let number_cards;
     MAIN FUNCTIONS
 ========================*/
 
-
+/* Number of cards on page load */
 function start_prompt() {
 
     number_cards = parseInt(prompt("Com quantas cartas você deseja jogar?"));
 
-    while ( isNaN(number_cards) || number_cards < 4 || number_cards > 14){
+    while (isNaN(number_cards) || number_cards < 4 || number_cards > 14) {
 
         number_cards = parseInt(prompt("Com quantas cartas você deseja jogar?"));
         console.log(`Number of cards: ${number_cards}`);
@@ -31,3 +35,15 @@ function start_prompt() {
 
 }
 
+function game_deck() {
+
+    for (i = 0; i < (number_cards / 2); i++) {
+        let card = card_options[i];
+
+        for (let j = 0; j < 2; j++) {
+            card_game.push(card);
+        }
+
+    }
+
+}
